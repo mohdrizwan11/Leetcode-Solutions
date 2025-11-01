27bb77dbdcc3fd82caf50d076ab65f26c4d12514
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2025-11-01 10:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (5.63%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) 
     {
-        int row = 0, col = matrix[0].length-1;
+        int row = matrix.length-1, col = 0;
 
-        //Staircase Search
-        while(row <= matrix.length-1 && col >= 0){
+        //Staircase Search Top & Right
+        while(row >= 0 && col <= matrix[0].length-1){
             if(matrix[row][col] == target){
                 return true;
             }
             else if(target < matrix[row][col]){
-                col--;
+                row--;
             }
             else{
-                row++;
+                col++;
             }
         }
         return false;
