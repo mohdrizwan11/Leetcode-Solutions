6390922691410be715857dbf232f8a1681f61549
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2025-11-01 09:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (8.45%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -3,19 +3,14 @@ class Solution {
     {
         int sum = 0;
         int n = mat.length;
-        int m = mat[0].length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                //primary
-                if (i == j) {
-                    sum += mat[i][j];
-                }
-                //secondary
-                else if (i + j == n - 1) {
-                    sum += mat[i][j];
-                }
-            }
+        for(int i = 0; i < n; i++)
+        {
+            //Primary Sum
+            sum += mat[i][i];
+            //Secondary Sum
+            if(i != n-1-i)
+                sum += mat[i][n-1-i];
         }
         return sum;
     }
