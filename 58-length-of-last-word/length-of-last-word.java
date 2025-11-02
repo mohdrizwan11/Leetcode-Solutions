288c6c86e101fe9f209ff4e5d6670c8adac7fc76
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2025-11-03 00:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (5.02%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String words[] = s.trim().split(" ");
+        s = s.trim(); // removes extra spaces at start and end.
         
-        return words[words.length-1].length();
+        int length = 0;
+        for(int i = s.length()-1; i >= 0; i--){
+            if(s.charAt(i) != ' '){
+                length++;
+            }
+            else{
+                break; // stop when you reach the space before the last word.
+            }
+        }
+        return length;
     }
 }
