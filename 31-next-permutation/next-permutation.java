2b04b4af92ca936e7b0723f9dd7939c3119518c9
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2025-11-29 15:33:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (36.31%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: find pivot
+        int i = n-2;
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+
+        // Step 2: find successor and swap
+        if(i >= 0){
+            int j = n-1;
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        
+        // Step 3: reverse right side
+        reverse(nums, i+1, n-1);
+
+    }
+    private void swap(int nums[], int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void reverse(int nums[], int left, int right){
+        while(left < right){
+            swap(nums, left++, right--);
+        }
+    }
+}
