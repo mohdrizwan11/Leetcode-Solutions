e8281e9b42a8dce11119af9e5258e89324ab2280
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2026-01-07 11:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (14.24%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+
+        Map<Node, Node> map = new HashMap<>();
+
+        Node newNode = new Node(head.val);
+        map.put(head, newNode);
+        Node oldTemp = head.next;
+        Node newTemp = newNode;
+
+        while(oldTemp != null){
+            Node copyNode = new Node(oldTemp.val);
+            map.put(oldTemp, copyNode);
+            newTemp.next = copyNode;
+            oldTemp = oldTemp.next;
+            newTemp = newTemp.next;
+        }
+
+        oldTemp = head;
+        newTemp = newNode;
+
+        while(oldTemp != null){
+            newTemp.random = map.get(oldTemp.random);
+            oldTemp = oldTemp.next;
+            newTemp = newTemp.next;
+        }
+        return newNode;
+    }
+}
