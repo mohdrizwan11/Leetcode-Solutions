f86ca4378535dc2dadd4cdaf4e947f0707d787e5
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2026-02-15 23:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (63.87%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.java
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean isCovered(int[][] ranges, int left, int right) {
-        boolean covered[] = new boolean[right-left+1];
-
-        for(int range[] : ranges){
-            int start = Math.max(range[0], left);
-            int end = Math.min(range[1], right);
-
-            for(int i = start; i <= end; i++){
-                covered[i - left] = true;
+        for(int i = left; i <= right; i++){
+            boolean found = false;
+            for(int range[] : ranges){
+                if(range[0] <= i && i <= range[1]){
+                    found = true;
+                    break;
+                }
             }
-        }
-
-        for(boolean c : covered){
-            if(!c) return false;
+            if(!found) return false;
         }
         return true;
     }
