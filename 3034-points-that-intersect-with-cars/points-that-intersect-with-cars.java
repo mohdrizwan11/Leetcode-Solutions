e9bbc66c564a6efa18fcf9b2eeb8ba382400d854
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2026-02-17 23:37:49 +0530

    Time: 2 ms (80.23%) | Memory: 46.2 MB (97.40%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -1,15 +1,20 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-        Set<Integer> set = new HashSet<>();
+        boolean covered[] = new boolean[101];
 
-        for(List<Integer> range : nums){
+        for(List<Integer> range: nums){
             int start = range.get(0);
             int end = range.get(1);
             for(int i = start; i <= end; i++){
-                set.add(i);
+                covered[i] = true;
             }
         }
 
-        return set.size();
+        int count = 0;
+        for(boolean point : covered){
+            if(point) count++;
+        }
+
+        return count;
     }
 }
