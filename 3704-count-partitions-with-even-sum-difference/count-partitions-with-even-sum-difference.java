8ca15452580db362cb7ef334c8dd335c8efe14c8
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2026-02-18 17:07:21 +0530

    Time: 1 ms (53.82%) | Memory: 44.1 MB (5.30%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,22 +1,23 @@
 class Solution {
     public int countPartitions(int[] nums) {
         int n = nums.length;
-        int leftSum[] = new int[n-1];
-        int rightSum[] = new int[n-1];
+        int leftSum[] = new int[n];
+        int rightSum[] = new int[n];
 
         leftSum[0] = nums[0];
-        for(int i = 1; i < n-1; i++){
+        for(int i = 1; i < n; i++){
             leftSum[i] = leftSum[i-1] + nums[i];
         }
 
-        rightSum[n-2] = nums[n-1];
-        for(int i = n-3; i >= 0; i--){
-            rightSum[i] = rightSum[i+1] + nums[i+1];
+        rightSum[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--){
+            rightSum[i] = rightSum[i+1] + nums[i];
         }
 
         int count = 0;
-        for(int i = 0; i < n-1; i++){
-            int differ = Math.abs(leftSum[i] - rightSum[i]);
+        
+        for(int i = 0; i < n - 1; i++){
+            int differ = Math.abs(leftSum[i] - rightSum[i+1]);
             if(differ % 2 == 0){
                 count++;
             }
