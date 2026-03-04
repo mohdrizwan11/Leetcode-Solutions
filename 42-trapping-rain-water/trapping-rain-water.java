54170771f50bb23d5fd0e3f16c591f2c4e8d0ef2
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2026-03-04 16:17:04 +0530

    Time: 1 ms (60.32%) | Memory: 48.6 MB (30.60%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,30 +1,25 @@
 class Solution {
-    public int trap(int[] height) 
-    {
+    public int trap(int[] height) {
         int n = height.length;
+        int leftBoundary[] = new int[n];
+        int rightBoundary[] = new int[n];
 
-        //Left Max Boundary Array
-        int leftMax[] = new int[n];
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        leftBoundary[0] = height[0];
+        for(int i = 1; i < n; i++){
+            leftBoundary[i] = Math.max(height[i], leftBoundary[i-1]);
         }
 
-        //Right Max Boundary Array
-        int rightMax[] = new int[n];
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        rightBoundary[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--){
+            rightBoundary[i] = Math.max(height[i], rightBoundary[i+1]);
         }
 
-        //Water Level => Min(leftMax, rightMax)
-        //Trapped Water = WaterLevl - Height
-        int tpWater = 0;
-        for (int i = 0; i < n; i++) {
-            int waterLevel = Math.min(leftMax[i], rightMax[i]);
-            tpWater += waterLevel - height[i];
+        int trapWater = 0;
+        for(int i = 0; i < n; i++){
+            int waterLevel = Math.min(leftBoundary[i], rightBoundary[i]);
+            trapWater += waterLevel - height[i];
         }
 
-        return tpWater;
+        return trapWater;
     }
 }
