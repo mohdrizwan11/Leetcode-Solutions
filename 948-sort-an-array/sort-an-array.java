93dab38212c48459e3d063e18de129319c34db64
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2026-03-23 00:05:29 +0530

    Time: 24 ms (71.59%) | Memory: 86.6 MB (28.85%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,20 +1,39 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        //Insertion Sort
-        int n = nums.length;
-
-        for(int i = 1; i < n; i++){
-            int curr = nums[i];
-            int prev = i-1;
-
-            while(prev >= 0 && nums[prev] > curr){
-                nums[prev+1] = nums[prev];
-                prev--;
-            }
-
-            nums[prev+1] = curr;
-        }
-
+        mergeSort(nums, 0, nums.length-1);
         return nums;
     }
+
+    private static void mergeSort(int nums[], int left, int right){
+        if(left < right){
+            int mid = left + (right - left) / 2;
+
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid+1, right);
+            merge(nums, left, mid, right);
+        }
+    }
+
+    private static void  merge(int nums[], int left, int mid, int right){
+        int temp[] = new int[right-left+1];
+        int i = left;
+        int j = mid+1;
+        int k = 0;
+
+        while(i <= mid && j <= right){
+            if(nums[i] < nums[j]){
+                temp[k++] = nums[i++];
+            }else{
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while(i <= mid) temp[k++] = nums[i++];
+        while(j <= right) temp[k++] = nums[j++];
+
+        for(int z = 0; z < temp.length; z++){
+            nums[left+z] = temp[z];
+        }
+    }
 }
+
