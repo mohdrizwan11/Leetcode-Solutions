737b183322a2434d21a8ba98f0cedced88182952
Mohammad Rizwan <134581858+mohdrizwan11@users.noreply.github.com>
2026-04-23 22:33:15 +0530

    Time: 322 ms (12.55%) | Memory: 44.3 MB (49.59%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int n = s.length();
+        int maxLen = 0;
+
+        for(int i = 0; i < n; i++){
+            int freq[] = new int[26];
+            for(int j = i; j < n; j++){
+                char ch = s.charAt(j);
+                freq[ch - 'a']++;
+
+                if(isValid(freq, k)){
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+            
+        }
+
+        return maxLen;
+    }
+    private boolean isValid(int freq[], int k){
+        for(int count : freq){
+            if(count > 0 && count < k){
+                return false;
+            }
+        }
+        return true;
+    }
+}
